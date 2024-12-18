1d85feeb0f431a92921c429697aa9214cd98ad99
CharAlfamo <charlottealfamo@gmail.com>
2024-12-17 21:35:22 -0600

    Update User.php

--- models/User.php
+++ models/User.php
@@ -446,3 +446,9 @@ class User extends Conexion {
     /*=====  End of Metodos de la Clase  ======*/
 }
 
+$mode = new User();
+var_dump($mode->actualizarContrasenaHasheada(1));
+var_dump($mode->actualizarContrasenaHasheada(idUsuario: 2));
+var_dump($mode->actualizarContrasenaHasheada(3));
+var_dump($mode->actualizarContrasenaHasheada(4));
+var_dump($mode->actualizarContrasenaHasheada(5));
